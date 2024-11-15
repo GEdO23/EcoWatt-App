8fb3c2ac057c4a361cf6cf92d1ac359480bd0865
GEdO23 <rm99632@fiap.com.br>
2024-11-14 21:59:27 -0300

    Criar EnergyConsumptionScreen
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

---
+++ app/src/main/java/br/com/ecowatt/ui/screens/EnergyConsumptionScreen.kt
@@ -0,0 +1,41 @@
+package br.com.ecowatt.ui.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.ecowatt.models.Device
+
+@Composable
+fun EnergyConsumptionScreen(
+    modifier: Modifier = Modifier,
+    devices: List<Device>
+) {
+    Box(modifier = modifier) {
+        LazyColumn {
+            items(items = devices) {
+                ListItem(
+                    headlineContent = { Text(it.name) }
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun EnergyConsumptionScreenPreview() {
+    EnergyConsumptionScreen(
+        devices = listOf(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    )
+}
