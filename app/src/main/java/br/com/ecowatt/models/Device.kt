1542e6b51f92ada2469db1df250c5a7058abef33
GEdO23 <rm99632@fiap.com.br>
2024-11-16 11:42:54 -0300

    Documentar Device
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

--- app/src/main/java/br/com/ecowatt/models/Device.kt
+++ app/src/main/java/br/com/ecowatt/models/Device.kt
@@ -2,6 +2,15 @@ package br.com.ecowatt.models
 
 import java.math.BigDecimal
 
+/**
+ * Data class representing a device.
+ *
+ * @property id The unique identifier of the device.
+ * @property name The name of the device.
+ * @property location The location where the device is installed.
+ * @property type The type/category of the device.
+ * @property consumptionLimit The consumption limit of the device.
+ */
 data class Device(
     val id: String,
     val name: String,
