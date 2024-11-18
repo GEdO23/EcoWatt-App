07202ca7626ce7e96f0acc2137fc29e4f909b28e
GEdO23 <rm99632@fiap.com.br>
2024-11-18 10:22:53 -0300

    Adcionar atributo consumptions e alerts ao modelo Device
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

--- app/src/main/java/br/com/ecowatt/models/Device.kt
+++ app/src/main/java/br/com/ecowatt/models/Device.kt
@@ -3,18 +3,22 @@ package br.com.ecowatt.models
 import java.math.BigDecimal
 
 /**
- * Data class representing a device.
+ * Data class representing a Device.
  *
  * @property id The unique identifier of the device.
  * @property name The name of the device.
  * @property location The location where the device is installed.
  * @property type The type/category of the device.
- * @property consumptionLimit The consumption limit of the device.
+ * @property consumptionLimit The maximum allowed consumption for the device.
+ * @property consumptions A list of consumption records associated with the device.
+ * @property alerts A list of alerts related to the device.
  */
 data class Device(
     val id: String,
     val name: String,
     val location: String,
     val type: String,
-    val consumptionLimit: BigDecimal
+    val consumptionLimit: BigDecimal,
+    val consumptions: List<Consumption>,
+    val alerts: List<Alert>
 )
--- app/src/main/java/br/com/ecowatt/sampledata/DeviceSampleData.kt
+++ app/src/main/java/br/com/ecowatt/sampledata/DeviceSampleData.kt
@@ -1,12 +1,47 @@
 package br.com.ecowatt.sampledata
 
+import br.com.ecowatt.models.Consumption
 import br.com.ecowatt.models.Device
 import java.math.BigDecimal
+import java.time.LocalDateTime
 
 val deviceSampleData = Device(
-    id = "id",
-    name = "name",
-    type = "type",
-    location = "location",
-    consumptionLimit = BigDecimal.ZERO
+    id = "-123",
+    name = "Eletric shower",
+    type = "Household appliance",
+    location = "Bathroom",
+    consumptionLimit = BigDecimal("356"),
+    consumptions = listOf(
+        Consumption(
+            id = "-5",
+            deviceId = "-123",
+            consumption = BigDecimal("124"),
+            dateTime = LocalDateTime.now()
+        ),
+        Consumption(
+            id = "-4",
+            deviceId = "-123",
+            consumption = BigDecimal("356"),
+            dateTime = LocalDateTime.now().minusMinutes(1)
+        ),
+        Consumption(
+            id = "-3",
+            deviceId = "-123",
+            consumption = BigDecimal("128"),
+            dateTime = LocalDateTime.now().minusMinutes(2)
+        ),
+        Consumption(
+            id = "-2",
+            deviceId = "-123",
+            consumption = BigDecimal("32"),
+            dateTime = LocalDateTime.now().minusMinutes(3)
+        ),
+        Consumption(
+            id = "-1",
+            deviceId = "-123",
+            consumption = BigDecimal("0"),
+            dateTime = LocalDateTime.now().minusMinutes(4)
+        ),
+    ),
+    alerts = emptyList()
 )
