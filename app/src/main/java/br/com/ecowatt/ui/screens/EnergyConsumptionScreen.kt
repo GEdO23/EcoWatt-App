7a55ae7aaa4bd761c8d8d72482eed988b2f1fd48
GEdO23 <rm99632@fiap.com.br>
2024-11-19 10:07:12 -0300

    Adcionar componente de alerta de lista vazia
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

--- app/src/main/java/br/com/ecowatt/ui/screens/EnergyConsumptionScreen.kt
+++ app/src/main/java/br/com/ecowatt/ui/screens/EnergyConsumptionScreen.kt
@@ -2,19 +2,22 @@ package br.com.ecowatt.ui.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.ecowatt.R
 import br.com.ecowatt.models.Device
-import br.com.ecowatt.sampledata.highConsumptionLevelDeviceSampleData
+import br.com.ecowatt.ui.components.consumption.EmptyListAlert
 import br.com.ecowatt.ui.components.consumption.EnergyConsumptionList
 import br.com.ecowatt.ui.theme.Azure500
 import br.com.ecowatt.ui.theme.EcoWattTheme
@@ -28,11 +31,20 @@ fun EnergyConsumptionScreen(
     onDeleteDevice: (deviceId: String) -> Unit
 ) {
     Box(modifier = modifier) {
-        EnergyConsumptionList(
-            devices = devices,
-            onClick = {},
-            onDeleteDevice = onDeleteDevice
-        )
+        if (devices.isEmpty()) {
+            EmptyListAlert(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 64.dp)
+                    .alpha(0.8F)
+            )
+        } else {
+            EnergyConsumptionList(
+                devices = devices,
+                onClick = {},
+                onDeleteDevice = onDeleteDevice
+            )
+        }
         FloatingActionButton(
             onClick = onCreateDevice,
             containerColor = Azure500,
@@ -57,11 +69,7 @@ private fun EnergyConsumptionScreenPreview() {
             Box(modifier = Modifier.padding(innerPadding)) {
                 EnergyConsumptionScreen(
                     modifier = Modifier.fillMaxSize(),
-                    devices = listOf(
-                        highConsumptionLevelDeviceSampleData,
-                        highConsumptionLevelDeviceSampleData,
-                        highConsumptionLevelDeviceSampleData
-                    ),
+                    devices = remember { mutableListOf<Device>() },
                     onCreateDevice = {},
                     onDeleteDevice = {}
                 )
