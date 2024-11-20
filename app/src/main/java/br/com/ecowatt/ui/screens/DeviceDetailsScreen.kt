4cac61bbeb103199e485569440225a5e0075d861
GEdO23 <rm99632@fiap.com.br>
2024-11-20 17:30:49 -0300

    Criar primeira versao da tela de detalhes de Device
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

---
+++ app/src/main/java/br/com/ecowatt/ui/screens/DeviceDetailsScreen.kt
@@ -0,0 +1,82 @@
+package br.com.ecowatt.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.ecowatt.R
+import br.com.ecowatt.models.Device
+import br.com.ecowatt.sampledata.lowConsumptionLevelDeviceSampleData
+import br.com.ecowatt.ui.components.consumption.ConsumptionLimitInfo
+import br.com.ecowatt.ui.components.consumption.ConsumptionReportsInfo
+import br.com.ecowatt.ui.components.consumption.EnergyConsumptionInfo
+import br.com.ecowatt.ui.components.device.DeviceHeader
+import br.com.ecowatt.ui.theme.Azure500
+import br.com.ecowatt.ui.theme.Neutral1000
+
+@Composable
+fun DeviceDetailsScreen(
+    modifier: Modifier = Modifier,
+    device: Device,
+    onClickEditDevice: () -> Unit
+) {
+    Box(modifier = modifier) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(32.dp)
+        ) {
+            DeviceHeader(
+                modifier = Modifier.fillMaxWidth(),
+                device = device
+            )
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                EnergyConsumptionInfo(
+                    modifier = Modifier.fillMaxWidth(),
+                    device = device
+                )
+                ConsumptionLimitInfo(
+                    modifier = Modifier.fillMaxWidth(),
+                    device = device
+                )
+                ConsumptionReportsInfo(
+                    modifier = Modifier.fillMaxWidth(),
+                    device = device,
+                    onClick = {}
+                )
+            }
+        }
+        FloatingActionButton(
+            onClick = onClickEditDevice,
+            containerColor = Azure500,
+            contentColor = Neutral1000,
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp)
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_edit),
+                contentDescription = ""
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+private fun DeviceDetailsScreenPreview() {
+    DeviceDetailsScreen(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .padding(16.dp),
+        device = lowConsumptionLevelDeviceSampleData,
+        onClickEditDevice = {}
+    )
+}
