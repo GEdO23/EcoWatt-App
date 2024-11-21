5b4aa64cf2b27f59cc5432a24b834d189b570ffc
GEdO23 <rm99632@fiap.com.br>
2024-11-21 20:09:40 -0300

    Criar método de cadastro com UserRepository e UserViewModel
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

--- app/src/main/java/br/com/ecowatt/models/User.kt
+++ app/src/main/java/br/com/ecowatt/models/User.kt
@@ -4,6 +4,21 @@ data class User(
     val name: String,
     val email: String,
     val password: String
+) {
+    fun toRequest(): UserRequest {
+        return UserRequest(
+            displayName = name,
+            email = email,
+            password = password
+        )
+    }
+}
+
+data class UserRequest(
+    val displayName: String,
+    val email: String,
+    val password: String,
+    val returnSecureToken: Boolean = true
 )
 
 fun emptyUser(): User = User(name = "", email = "", password = "")
---
+++ app/src/main/java/br/com/ecowatt/repository/UserRepository.kt
@@ -0,0 +1,44 @@
+package br.com.ecowatt.repository
+
+import br.com.ecowatt.models.User
+import com.google.gson.Gson
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.IOException
+
+class UserRepository {
+    private val gson = Gson()
+    private val httpClient = OkHttpClient()
+
+    private val apiKey = "AIzaSyDPN4xpjTmo-eNuJs-4UnItxMGUId4ePwQ"
+
+    fun signUp(
+        user: User,
+        onRequestFailure: (e: IOException) -> Unit,
+        onRequestSuccess: () -> Unit
+    ) {
+        val url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$apiKey"
+        val json = gson.toJson(user.toRequest())
+        val contentType = "application/json".toMediaType()
+        val body = json.toRequestBody(contentType)
+
+        val request = Request.Builder()
+            .url(url)
+            .post(body)
+            .build()
+
+        val response = object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                onRequestFailure(e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                onRequestSuccess()
+            }
+        }
+
+        httpClient.newCall(request)
+            .enqueue(response)
+    }
+}
