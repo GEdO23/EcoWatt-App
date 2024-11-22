a8d587a68b64813e4389ef6ee764a4f3eb82fa76
GEdO23 <rm99632@fiap.com.br>
2024-11-22 09:08:35 -0300

    Adcionar keyboardOptions e visualTransformation aos formularios de autenticacao
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

--- app/src/main/java/br/com/ecowatt/ui/screens/user/SignupScreen.kt
+++ app/src/main/java/br/com/ecowatt/ui/screens/user/SignupScreen.kt
@@ -1,6 +1,7 @@
 package br.com.ecowatt.ui.screens.user
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -8,6 +9,10 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.ecowatt.models.User
@@ -28,21 +33,37 @@ fun SignupScreen(
             modifier = Modifier.fillMaxWidth(),
             value = user.value.name,
             onValueChange = { user.value = user.value.copy(name = it) },
-            label = { Text("Name") }
+            label = { Text("Name") },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Words,
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            )
         )
-        
+
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
             value = user.value.email,
             onValueChange = { user.value = user.value.copy(email = it) },
-            label = { Text("Email") }
+            label = { Text("Email") },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.None,
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            )
         )
 
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
             value = user.value.password,
             onValueChange = { user.value = user.value.copy(password = it) },
-            label = { Text("Password") }
+            label = { Text("Password") },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.None,
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Send
+            ),
+            visualTransformation = PasswordVisualTransformation()
         )
 
         Button(
