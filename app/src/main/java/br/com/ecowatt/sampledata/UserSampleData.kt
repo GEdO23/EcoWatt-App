5c048adc427cfe307372073cd2cdea7cee4ba0fd
GEdO23 <rm99632@fiap.com.br>
2024-11-22 08:49:47 -0300

    Adcionar UserSampleData
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

---
+++ app/src/main/java/br/com/ecowatt/sampledata/UserSampleData.kt
@@ -0,0 +1,11 @@
+package br.com.ecowatt.sampledata
+
+import br.com.ecowatt.models.User
+
+object UserSampleData {
+    val gabriel = User(
+        name = "Gabriel",
+        email = "gabriel@gmail.com",
+        password = "mypassword1234"
+    )
+}
