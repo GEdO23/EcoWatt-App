d868dd9a3d399ac726289ba7af8c372f909faad8
GEdO23 <rm99632@fiap.com.br>
2024-11-22 08:51:06 -0300

    Adcionar novas telas ao enum Screen
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

--- app/src/main/java/br/com/ecowatt/navigation/Screen.kt
+++ app/src/main/java/br/com/ecowatt/navigation/Screen.kt
@@ -4,9 +4,14 @@ package br.com.ecowatt.navigation
  * Enum class representing the different screens in the EcoWatt app.
  *
  * Screens:
+ * * [WELCOME]
+ * * [SIGNUP]
+ * * [LOGIN]
  * * [HOME]
  * * [ENERGY_CONSUMPTION]
  * * [REGISTER_DEVICE]
+ * * [DEVICE_DETAILS]
+ * * [UPDATE_DEVICE]
  *
  * @property title The title of the screen.
  */
@@ -14,37 +19,42 @@ enum class Screen(
     val title: String? = null,
 ) {
     /**
+     * Welcome screen.
+     */
+    WELCOME,
+
+    /**
+     * Sign-up screen.
+     */
+    SIGNUP("Sign up"),
+
+    /**
+     * Login screen.
+     */
+    LOGIN("Login"),
+
+    /**
      * Home screen.
-     *
-     * @sample br.com.ecowatt.ui.screens.HomeScreen
      * */
     HOME("Home"),
 
     /**
      * Energy consumption screen.
-     *
-     * @sample br.com.ecowatt.ui.screens.EnergyConsumptionScreen
      * */
     ENERGY_CONSUMPTION,
 
     /**
      * Screen for registering a new device.
-     *
-     * @sample br.com.ecowatt.ui.screens.FormDeviceScreen
      * */
     REGISTER_DEVICE("Register device"),
-    
+
     /**
      * Screen for displaying device details.
-     * 
-     * @sample br.com.ecowatt.ui.screens.DeviceDetailsScreen
      */
     DEVICE_DETAILS,
-    
+
     /**
      * Screen for updating an device.
-     *
-     * @sample br.com.ecowatt.ui.screens.FormDeviceScreen
      */
     UPDATE_DEVICE("Update device")
 }
