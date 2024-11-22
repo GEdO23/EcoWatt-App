27fb26f655ef71d27385f74022dfd6981c985df3
GEdO23 <rm99632@fiap.com.br>
2024-11-22 10:38:28 -0300

    Documentar User model e DTO de Login e Signup
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

--- app/src/main/java/br/com/ecowatt/models/User.kt
+++ app/src/main/java/br/com/ecowatt/models/User.kt
@@ -1,5 +1,11 @@
 package br.com.ecowatt.models
 
+/**
+ * Data class representing a user.
+ *
+ * @property displayName The display name of the user.
+ * @property email The email of the user.
+ */
 data class User(
     val displayName: String,
     val email: String
